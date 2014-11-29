62a1ba3c3f26e03a1da419b974933fd37e91a209
Mary <Mary@Mary>
2014-11-29 20:46:02 +0200

    Session

---
+++ WebHandMade/src/java/session/MessagesFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package session;
+
+import entity.Messages;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Mary
+ */
+@Stateless
+public class MessagesFacade extends AbstractFacade<Messages> {
+    @PersistenceContext(unitName = "WebHandMadePU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public MessagesFacade() {
+        super(Messages.class);
+    }
+    
+}
